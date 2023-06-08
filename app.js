9a20fcf21a7bd1b36973fd884f365a03e6e3fcf0
jolly01008 <jollgmll292@gmail.com>
2023-06-08 23:45:14 +0800

    feat: add handlebars & bootstrap

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 
-app.get('/',(req , res)) => {
+app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
+app.set('view engine','hbs')
+
+app.get('/',(req , res) => {
   res.render('index')
-}
+})
 
 app.listen( port, () => {
   console.log(`App is running on  http://localhost:${port}`)
