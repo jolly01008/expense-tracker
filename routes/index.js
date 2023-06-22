aefc7b3343f74d93f991c7edb72b157135ee77a7
jolly01008 <jollgmll292@gmail.com>
2023-06-22 18:23:34 +0800

    feat: add authenticator in routes & logout feature

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const users = require('./modules/users')
 const records = require('./modules/records')
 const home = require('./modules/home')
+const { authenticator } = require('../middleware/auth')
 
-router.use( '/users' , users )
-router.use( '/records' , records )
-router.use( '/' , home )
+router.use( '/records' , authenticator , records )
+router.use( '/users'  , users )
+router.use( '/' , authenticator , home )
 
 
 module.exports = router
