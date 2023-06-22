5c9635acd34cd8d58fa58a04391960204d333300
jolly01008 <jollgmll292@gmail.com>
2023-06-22 17:44:08 +0800

    feat:add express-session & login feature via passport

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 app.use(express.urlencoded({ extended:true })) 
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine','hbs')
 
+app.use(session({
+  secret:'ThisisMySecret',
+  resave: false,
+  saveUninitialized: true
+ }))
+
+usePassport(app)
+
 app.use(routes)
 
-// app.get('/',(req , res) => {
-//   res.render('index')
-// })
+
 
 app.listen( port, () => {
   console.log(`App is running on  http://localhost:${port}`)
