186e35d40e40dd140afd30c9ed92c8cbfe08584e
jolly01008 <jollgmll292@gmail.com>
2023-06-23 16:55:48 +0800

    modify home router and index.hbs to view all records

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/' , (req,res)=>{
-  res.render('index')
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
+router.get('/' , async (req,res) => {
+  // Record.find({})
+  //   .lean()
+  //   .sort({ _id: 'desc' })
+  //   .then(records => res.render('index' , { records }))
+  //   .then(records =>console.log('records:' , records))
+  //   .catch(err => console.log(err))
+  const records = await Record.find().lean().sort({date:"desc"})
+  const formattedRecords = []
+  let totalAmount = 0
+  for(const record of records ){
+    const category = await Category.findById(record.categoryId).lean()
+    const formattedDate = new Date(record.date).toISOString().slice( 0 , 10 )
+    totalAmount += record.amount
+    formattedRecords.push({...record , categoryIcon: category.icon , date: formattedDate})
+    console.log('formattedRecords:',formattedRecords)
+  }
+  res.render('index',{ records: formattedRecords , totalAmount })
 })
 
 
