98324b5d3fe7a099708279492a2d7210c651e2eb
jolly01008 <jollgmll292@gmail.com>
2023-07-11 22:52:14 +0800

    add category filter feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,7 +11,6 @@ router.get('/' , async (req,res) => {
   return Category.find()
     .lean()
     .then((categories) => {
-      console.log('幹嘛用這個categories:',categories)
       return Record.find({userId})
         .populate('categoryId')  //以categoryId欄位，把Record跟Category資料庫關聯
         .lean()
@@ -21,12 +20,38 @@ router.get('/' , async (req,res) => {
             record.date = record.date.toISOString().slice(0, 10)
             totalAmount += record.amount
           })
-          return res.render('index' , { records,  totalAmount })
+          return res.render('index' , { records,  totalAmount ,categories})
         })
         .catch(err => console.log(err))
     })
 })
 
+// 下拉式選單按類別排序
+router.post('/', (req, res) => {
+  const userId = req.user._id
+  const { categoryId } = req.body
+  if (categoryId === "all") {
+    return res.redirect('/')
+  }
+  return Category.find()
+    .lean()
+    .then((categories) => {
+      return Record.find({ userId, categoryId })
+        .populate('categoryId') //以'categoryId'欄位把Record跟Category資料庫關聯
+        .lean()
+        .sort({ date: 'desc' })
+        .then((records) => {
+          let totalAmount = 0
+          records.forEach((record) => {
+            totalAmount += record.amount
+            record.date = record.date.toISOString().slice(0,10)
+          })
+          return res.render('index', { records, categories, totalAmount })
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+})
 
 module.exports = router
 
