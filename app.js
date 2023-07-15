829c7eaae3e9ced9929a614801f9aca8d9aedf88
jolly01008 <jollgmll292@gmail.com>
2023-07-15 18:13:59 +0800

    add message in login and register feature via connect-flash

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
@@ -26,9 +27,12 @@ app.use(session({
 app.use(methodOverride('_method'))
 usePassport(app)
 
+app.use(flash())
 app.use((req,res,next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user   //req.user是反序列化取出的user資訊
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,11 +19,35 @@ router.get('/register',( req,res ) => {
 
 router.post('/register',( req,res ) => {
   const { name,email,password,confirmPassword } = req.body
+  const errors = []
+  if( !name || !email || !password || !confirmPassword ){
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if( password !== confirmPassword ){
+    errors.push({ message: '密碼與確認密碼不相符' })
+  }
+  if(errors.length){
+    return res.render('register',{
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
+  //檢查使用者是否已經註冊
   User.findOne({ email })
     .then(user => {
       if(user){ 
-        console.log('這位使用者已經註冊過了')
-        return res.render('register',{ name , email , password , confirmPassword })
+        errors.push({message: '這位使用者已經註冊過了'})
+        return res.render('register',{ 
+          errors , 
+          name , 
+          email , 
+          password , 
+          confirmPassword 
+        })
       }
         return bcrypt
           .genSalt(10)
@@ -33,13 +57,15 @@ router.post('/register',( req,res ) => {
             email,
             password : hash
           }))
+          .then(() => res.redirect('/'))
+          .catch(error => console.log(error))
     })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch(err => console.log(err))
 })
 
 router.get('/logout', (req,res) => {
   req.logout()
+  req.flash('success_msg','你已經成功登出!')
   res.redirect('/users/login')
 })
 
